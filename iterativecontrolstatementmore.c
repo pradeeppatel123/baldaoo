70e455559a91609f5854f425e05d59c19bec734f
pradeeppatel123 <111377568+pradeeppatel123@users.noreply.github.com>
2022-08-16 23:15:12 +0530

    Add files via upload

---
+++ iterativecontrolstatementmore.c
@@ -0,0 +1,52 @@
+//1. Write a program to find the Nth term of the Fibonnaci series. 
+/*#include<stdio.h>
+int main()
+{
+    int n,i,a=-1,b=1,c;
+    printf("Enter a number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+         c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    
+}*/
+
+
+//2. Write a program to print first N terms of Fibonacci series
+
+/*#include<stdio.h>
+int main()
+{
+    int n,a=-1,b=1,c,i;
+    printf("Enter a number ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        c=a+b;
+        printf("first %d terms of fibonacci series is : ",n);
+        printf("%d\n",c);
+        a=b;
+        b=c;
+    }
+    return 0;
+}*/
+
+/*3. Write a program to check whether a given number is there in the Fibonacci
+series or not.*/
+
+
+
+//4. Write a program to calculate HCF of two numbers
+/*5. Write a program to check whether two given numbers are co-prime
+numbers or not */
+//6. Write a program to print all Prime numbers under 100
+//7. Write a program to print all Prime numbers between two given numbers
+//8. Write a program to find next Prime number of a given number
+/*9. Write a program to check whether a given number is an Armstrong number
+or not */
+//10. Write a program to print all Armstrong numbers under 1000
